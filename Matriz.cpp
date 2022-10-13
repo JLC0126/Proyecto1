ae4ef3c60daaf5d34c76894fcf5e41c18b955d4b
Jose Castillo <jcastillor@unah.hn>
2022-10-13 16:06:26 -0600

    Inicia la implementacion de la funciones miembro

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,2 +1,16 @@
 #include 'Matriz.hpp'
 
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+Matriz::Matriz(int f, int c){
+    fila=f;
+    columna=c;
+}
+
+Matriz Matriz::multiplicar(Matriz *m1, Matriz *m2){
+
+    
+}
--- Matriz.hpp
+++ Matriz.hpp
@@ -1,11 +1,11 @@
 
 class Matriz{
     public:
-        Matriz(double, double);
-        Matriz multiplicar(Matriz, Matriz);
-        Matriz suma(Matriz, Matriz);
-        Matriz escalar(Matriz );
-        double ProductoPunto(Matriz, Matriz);
+        Matriz(int, int);
+        Matriz multiplicar(Matriz *, Matriz * );
+        Matriz suma(Matriz *, Matriz *);
+        Matriz escalar(Matriz * );
+        double ProductoPunto(Matriz *, Matriz * );
 
     private:
         double fila,columna;
